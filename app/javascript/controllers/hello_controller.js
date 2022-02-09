df6355d02a796fb355998a88d4d2f4cf9018aecb
Vadim Constantin <vad.constantin@gmail.com>
2022-02-09 17:54:49 +0100

    javascript for HOME is done

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -10,9 +10,46 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "output" ]
 
   connect() {
-    this.outputTarget.textContent = 'Hello, Stimulus!'
+    console.log("Hello, Stimulus!")
+
+    const nav = document.querySelector(".navbar-lewagon")
+    const logo = document.querySelector(".nav-link")
+    const links = document.querySelectorAll(".dropdown-item")
+
+    console.log(links)
+
+    window.addEventListener("scroll", () => {
+      console.log(window.scrollY)
+      if (window.scrollY > 100 && window.scrollY < 611 ) {
+        nav.style = "background: transparent"
+        logo.style = "color: transparent"
+        links.forEach((link) => {
+          link.style = "opacity: 0"
+        })
+         }
+
+      else if (window.scrollY > 611) {
+        nav.style = "background: rgb(255, 234, 221)"
+        logo.style = "opacity: 1"
+        links.forEach((link) => {
+          link.style = "opacity: 1"
+        })
+
+       }
+
+      else {
+        nav.style = "background: rgb(255, 234, 221)"
+        logo.style = "opacity: 1"
+        links.forEach((link) => {
+          link.style = "opacity: 1"
+        })
+
+      }
+    })
+
   }
+
+
 }
